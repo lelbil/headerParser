aa3016260dd46c84f23cda11edc9924dec5fbed9
LelBil <billelattouchi@yahoo.fr>
2017-06-10 04:51:36 +0200

    deleted logs

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ var requestIp = require('request-ip');
 var app = express();
 
 app.get('/', function(req, res) {
-	//console.log(req.body);
 	var 	headers = req.headers,
 		lang = req.headers['accept-language'].slice(0, 5),
 		computer = req.headers['user-agent'].match(/\(([^)]+)\)/)[1],
-		ip = requestIp.getClientIp(req).slice(7);//req.connection.remoteAddress.slice(7);
-	//res.send(require('util').inspect(req));
+		ip = requestIp.getClientIp(req);
 	var ret = {
 		"language" : lang, 
 		"computer" : computer, 
