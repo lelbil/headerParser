1a4b0ca3680aae58f9d91b61c6d2ff0095fc1b45
LelBil <billelattouchi@yahoo.fr>
2017-06-10 04:36:01 +0200

    initial commit, working version

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	//console.log(req.body);
+	var 	headers = req.headers,
+		lang = req.headers['accept-language'].slice(0, 5),
+		computer = req.headers['user-agent'].match(/\(([^)]+)\)/)[1],
+		ip = req.connection.remoteAddress.slice(7);
+	//res.send(require('util').inspect(req));
+	var ret = {
+		"language" : lang, 
+		"computer" : computer, 
+		"ip" : ip
+	};
+	res.json(ret);
+});
+
+app.listen(process.env.PORT || 8080);
+console.log("App running..");
